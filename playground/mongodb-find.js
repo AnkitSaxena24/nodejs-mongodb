6197b9162d00510ef3456c4d477738e13bd8c0d7
AnkitSaxena24 <ankitsaxena202@hotmail.com>
2018-12-02 22:23:18 +0530

    new code

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -9,20 +9,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	console.log('Connected to MongoDB server');
 	const db = client.db('TodoApp'); //connecting to the database
 
-	/*db.collection('Todos').find({//finding based on conditions
+	db.collection('Todos').find({//finding based on conditions
 		_id: new ObjectID('5c039713ca871c32eccf5911')
 	}).toArray().then((docs) => {
 		console.log('Todos');
 		console.log(JSON.stringify(docs, undefined, 2));
 	}, (err) => {
 		console.log('Unable to handle request');
-	});*/
+	});
 
-	/*db.collection('Todos').find().count().then((count) => {//finding the count
+	db.collection('Todos').find().count().then((count) => {//finding the count
 		console.log(`Todos count: ${count}`);
 	}, (err) => {
 		console.log('Unable to find the count');
-	});*/
+	});
 
 	db.collection('Users').find({name: 'Ankit'}).toArray().then((names) => {//finding names
 		console.log(`Searched names: ${JSON.stringify(names, undefined, 2)}`);
