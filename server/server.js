72ad8595e126b5d41dac0c5b07efbc2e0580837d
AnkitSaxena24 <ankitsaxena202@hotmail.com>
2018-12-03 01:02:07 +0530

    Models with validation

--- server/server.js
+++ server/server.js
@@ -3,22 +3,28 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {//defining the properties of a collection by the model - Todo
+//Model Todo
+/*var Todo = mongoose.model('Todo', {//defining the properties of a collection by the model - Todo
 	text: {
-		type: String
+		type: String,
+		required: true, 
+		minlength: 1,
+		trim: true
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		defualt: false
 	},
 	completedAt: {
-		type: Number
+		type: Number,
+		default: null
 	}
 });
 
 
 //creating the entry in Todo collection
 var newTodo = new Todo({
-	text: 'Drive a car'
+	text: '  Drive a car  '
 });
 
 //saving and priting the entry
@@ -26,4 +32,24 @@ newTodo.save().then((doc) => {
 	console.log(`Saved Todo: ${doc}`);
 }, (err) => {
 	console.log('Something went wrong');
+});
+*/
+//Model User
+var User = mongoose.model('User', {
+	email: {
+		type: String, 
+		required: true, 
+		minlength: 1,
+		trim: true
+	}
+});
+
+var newUser = new User({
+	email: 'ankitsaxena807@hotmail.com'
+});
+
+newUser.save().then((email) => {
+	console.log(`User email is: ${email}`);
+}, (err) => {
+	console.log('Unable to save email');
 });
